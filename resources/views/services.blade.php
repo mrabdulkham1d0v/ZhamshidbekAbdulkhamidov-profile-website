d375c6f22e1726fb4c965bbee14edfb928ee87e7
Zhamshidbek Abdulkhamidov <190103308@stu.sdu.edu.kz>
2021-02-22 15:51:40 +0600

    adding a services blade

---
+++ resources/views/services.blade.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=1, initial-scale=1.0">
+    <link rel = stylesheet href = "{{ asset('style.css') }}">
+    <title>Services</title>
+</head>
+<body>
+    <section class="services" id='services'>
+        <div class="heading white">
+            <h2>Our Services</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+        </div>
+        <div class="content">
+            <div class="servicesBx">
+                <img src="./images/icon1.png" alt="">
+                <h2>Wed Design</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta maiores incidunt quae laudantium, tempore reprehenderit ducimus odio ratione aliquid?</p>
+            </div>
+            <div class="servicesBx">
+                <img src="./images/icon2.png" alt="">
+                <h2>Wed Development</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta maiores incidunt quae laudantium, tempore reprehenderit ducimus odio ratione aliquid?</p>
+            </div>
+            <div class="servicesBx">
+                <img src="./images/icon3.png" alt="">
+                <h2>App Developing</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta maiores incidunt quae laudantium, tempore reprehenderit ducimus odio ratione aliquid?</p>
+            </div>
+            <div class="servicesBx">
+                <img src="./images/icon4.png" alt="">
+                <h2>Photo Editing</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta maiores incidunt quae laudantium, tempore reprehenderit ducimus odio ratione aliquid?</p>
+            </div>
+            <div class="servicesBx">
+                <img src="./images/icon5.png" alt="">
+                <h2>Copyrighting</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta maiores incidunt quae laudantium, tempore reprehenderit ducimus odio ratione aliquid?</p>
+            </div>
+            <div class="servicesBx">
+                <img src="./images/icon6.png" alt="">
+                <h2>Film Making</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta maiores incidunt quae laudantium, tempore reprehenderit ducimus odio ratione aliquid?</p>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
