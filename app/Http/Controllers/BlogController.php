150cf05448d0e40d099a7a5e658219014a72b8a3
Zhamshidbek Abdulkhamidov <190103308@stu.sdu.edu.kz>
2021-03-15 23:35:29 +0600

    Creating a store function

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -13,5 +13,14 @@ class BlogController extends Controller
 
         return view('blog.index')->with(['posts'=>$blogs]);
     }
+
+    public function store(Request $request){
+        Post::create([
+            "title" => $request -> title,
+            "body" => $request -> body
+        ]);
+
+        return back();
+    }
     
 }
--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -7,9 +7,11 @@
     <title>Create Blog</title>
 </head>
 <body>
-    <form action="" method="POST">
+    <form action="{{ route('add-post')}}" method="POST">
+        @csrf 
         <input type="text" name="title" placeholder="title">
         <input type="text" name="body" placeholder="body">
+        <button type="submit">Create</button>
     </form>
 </body>
 </html>
